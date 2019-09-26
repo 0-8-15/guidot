04f413218becc241712e5850574b387a866cb942
Jörg F. Wittenberger <Joerg.Wittenberger@softeyes.net>
2019-09-26 21:25:07 +0200

    Factor jscheme code out into its own file.

--- front/apps/front/main.scm
+++ front/apps/front/main.scm
@@ -36,35 +36,7 @@ NULL;
 #endif
 ")))
 
-(define jscheme-invoke/s2s
-  (c-lambda (char-string) char-string "
-#ifdef ANDROID
-extern const char *jscheme_eval(const char *);
-#endif
-___result=
-#ifdef ANDROID
-(char*) jscheme_eval(___arg1);
-#else
-NULL;
-#endif
-"))
-
-(define jscheme-call
-  ;; Not sure that we need a mutex here.  But what if the java side
-  ;; manages to call into gambit?
-  (let ((mutex (make-mutex 'jscheme)))
-    (define (jscheme-call obj)
-      (let* ((s (let ((req (object->string obj)))
-                  (mutex-lock! mutex)
-                  (jscheme-invoke/s2s req)))
-             (r0 (begin
-                   (mutex-unlock! mutex)
-                   (call-with-input-string s read))))
-        (cond
-         ;; Numbers are always printed as inexacts by jscheme.
-         ((integer? r0) (inexact->exact r0))
-         (else r0))))
-  jscheme-call))
+(include "jscheme.scm")
 
 (cond-expand
  (android
