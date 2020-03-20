11202e5e9456c5fe343aa07545ee6e07ec0729f8
Jörg F. Wittenberger <Joerg.Wittenberger@softeyes.net>
2020-03-20 20:45:33 +0100

    Add forgotten file

---
+++ modules/likely/2001-basic.scm
@@ -0,0 +1,95 @@
+(define x (make-observable 23 #f #f 'X))
+
+(test-error
+ "no current transaction"
+ (observable-set! x 42))
+
+(test-assert
+ "set! works"
+ (begin
+   (parameterize (($implicit-current-transactions #t)) (observable-set! x 42))
+   (= (observable-deref x) 42)))
+
+(test-assert
+ "with-current-transaction"
+ (with-current-transaction
+  (lambda ()
+    (let ((y (observable-deref (observable-reference x))))
+      (observable-set! x (+ y 3))
+      #t))))
+(define y (make-observable 42))
+(define op (make-observable *))
+(define res (make-observable 'undefined))
+
+(test-assert
+ "observable-invoke!"
+ (parameterize
+  (($implicit-current-transactions #t))
+  (observable-invoke!
+   (lambda (res op a b) (observable-set! res ((observable-deref op) (observable-deref a) (observable-deref b))))
+   (list res op x y))))
+
+(test-assert "observable-alter!" (eq? (with-current-transaction (lambda () (observable-alter! res + 10))) 1900))
+
+(define observe-conflict-set!
+  (dynamic
+   (lambda (r v)
+     (parameterize (($implicit-current-transactions #t)) (observable-set! r v)))))
+
+(define (add-and-set-conflict ref overwrite)
+  (lambda (a b) (observe-conflict-set! ref overwrite) (+ a b)))
+
+(test-assert
+ "observable-alter! with conflict (retries)"
+ (eq? (parameterize
+       (($stm-retry-limit 2))
+       (with-current-transaction
+        (lambda ()
+          (observable-alter! res (add-and-set-conflict res 5) 10))))
+      15))
+
+(test-error
+ "$stm-retry-limit 0: retries become exceptions"
+ (eq? (parameterize
+       (($stm-retry-limit 0))
+       (with-current-transaction
+        (lambda ()
+          (observable-alter! res (add-and-set-conflict res 0) 10))))
+      10))
+
+(test-assert
+ "Outdated reference (currently only reported)"
+ (eq? (parameterize
+       (($stm-retry-limit 3))
+       (observe-conflict-set! res 42)
+       (with-current-transaction
+        (lambda ()
+          (observe-conflict-set! res 7)
+          (observable-alter! res + #;(add-and-set-conflict res 7) 23))))
+      30))
+
+(test-error
+ "Outdated reference and conflict with zero $stm-retry-limit fails."
+ (eq? (parameterize
+       (($stm-retry-limit 0))
+       (observe-conflict-set! res 42)
+       (with-current-transaction
+        (lambda ()
+          (observe-conflict-set! res 11)
+          (observable-alter! res (add-and-set-conflict res 7) 23))))
+      30))
+
+(test-error
+ "Retry limit effective"
+ (eq? (parameterize
+       (($stm-retry-limit 10))
+       (observe-conflict-set! res 42)
+       (with-current-transaction
+        (lambda ()
+          (observe-conflict-set! res 11)
+          (observable-alter! res (add-and-set-conflict res 7) 23))))
+      30))
+
+;; ($implicit-current-transactions #f)
+
+;;(run-tests)
