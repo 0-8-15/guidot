63952f05e38084f1962646810961e5200a64190f
Jörg F. Wittenberger <Joerg.Wittenberger@softeyes.net>
2020-05-03 16:24:09 +0200

    ZT: keep currently unused code around

--- modules/zerotiercore/zerotiercore-extensions.scm
+++ modules/zerotiercore/zerotiercore-extensions.scm
@@ -21,6 +21,29 @@ c-declare-end
     (zt-prm-zt %%zt-prm) lsock id addr))
   addr)
 
+#|
+;; Might be usedful.
+bool ZT_Node_setConfigItem(ZT_Node* node, uint64_t nwid, int item, uint64_t value)
+{
+  try {
+    ZeroTier::SharedPtr<ZeroTier::Network> net = reinterpret_cast<ZeroTier::Node *>(node)->network(nwid);
+    if(!net->hasConfig()) {
+      fprintf(stderr, "ZT_Node_setConfigItem: network %xll has no config\n", nwid);
+      return false;
+    }
+    ZeroTier::NetworkConfig &cfg = (ZeroTier::NetworkConfig &) net->config();
+    // Hm. FIXME.  Maybe this would need locking.  Function first, it's an experiment.
+    switch(item) {
+    case 1: cfg.mtu = value; return true;
+    case 2: cfg.multicastLimit = value; return true;
+    }
+    return false;
+  } catch ( ... ) {
+    return false;
+  }
+}
+;;; ------
+
 (c-declare "extern bool ZT_Node_setConfigItem(ZT_Node* node, uint64_t nwid, int item, uint64_t value);")
 
 (define (zt-set-config-item! nwid item value)
@@ -28,3 +51,4 @@ c-declare-end
   (begin-zt-exclusive
    ((c-lambda (zt-node unsigned-int64 int unsigned-int64) bool "ZT_Node_setConfigItem")
     (zt-prm-zt %%zt-prm) nwid item value)))
+|#
