d888392341de69500e05d61eec3dbeacf9ae5986
Jörg F. Wittenberger <Joerg.Wittenberger@softeyes.net>
2021-01-14 20:46:09 +0100

    GUIDE: adolescent bg+fg pair rendering

--- modules/guide/guide-button.scm
+++ modules/guide/guide-button.scm
@@ -5,6 +5,174 @@
   ;; default: #f
   (make-parameter #f))
 
+(define (MATURITY+2:make-guide-bg+fg-view) ;; a PAIR (BG . FG)
+  (define (color-conv color)
+    (cond ;; TBD: define consistent conversion here!
+     ((integer? color) (make-rect-single-color-array color))
+     ((not color) guide-color-transparent+black-array)
+     (else color)))
+  ;; DEV: derivations; maybe useless
+  (define foreground-x (lambda () 0.)) ;; example
+  (define foreground-y (lambda () 0.)) ;; example
+  (define foreground-w (lambda () 0.)) ;; example
+  (define foreground-h (lambda () 0.)) ;; example
+  (define figure ;; rectangular ??? required at all? ???
+    (let* ((formula (lambda (w h) (make-mdvector-rect-vertices/x0y0x1y1 0 0 w h)))
+           (calc (memoize-last formula eqv? eqv?)))
+      (lambda () (calc w h))))
+  (let ((background #f) ;; entire area
+        (foreground #f) ;; client area
+        (horizontal-align 'left)
+        (vertical-align 'bottom)
+        (x 0)
+        (y 0)
+        (w 100)
+        (h 100)
+        ;;; TBD add space for padding&border?
+        ;;;
+        ;;; NONONO Instead have a style!
+        ;;;
+        (scale #f)
+        (shift #f)
+        (rot #f)
+        (visible #t))
+    (define normalize
+      ;; TBD: normalizer for x,y to scaled,shifted,rotated,[visible]
+      ;; client area.
+      NYI)
+    (define fixed-draw
+      (lambda ()
+        (and visible (or background foreground)
+             (let ((shift-0 (and shift (vector-ref shift 0)))
+                   (shift-1 (and shift (vector-ref shift 1)))
+                   (shift-2 (and shift (vector-ref shift 2)))
+                   (scale-0 (and scale (vector-ref scale 0)))
+                   (scale-1 (and scale (vector-ref scale 1)))
+                   (scale-2 (and scale (vector-ref scale 2)))
+                   (rot-0 (and rot (vector-ref rot 0)))
+                   (rot-1 (and rot (vector-ref rot 1)))
+                   (rot-2 (and rot (vector-ref rot 2)))
+                   (rot-3 (and rot (vector-ref rot 3)))
+                   ;;;
+                   ;;; NONONO Instead have a style!
+                   ;;;
+                   #|
+                   (background (if (procedure? background) (background) background))
+                   (foreground (if (procedure? foreground) (foreground) foreground))
+                   |#
+                   (foreground-w (foreground-w))
+                   (foreground-h (exact->inexact (foreground-h)))
+                   (foreground-x (foreground-x))
+                   (foreground-y (exact->inexact (foreground-y)))
+                   ) ;; no more precalculations
+               (lambda () ;; result … TBD: specialize up to nothing…
+                 ;; if not already in shift mode ... simplicity: a toplevel matrix push
+                 (glPushMatrix)
+                 (when shift-0
+                   (glTranslatef//checks shift-0 shift-1 shift-2))
+                 (when scale-0
+                   (glScalef//checks scale-0 scale-1 scale-2))
+                 (when rot-0
+                   (glRotatef//checks rot-0 rot-1 rot-2 rot-3))
+                 (cond
+                  ((procedure? background) (background))
+                  (background
+                   (MATURITY+1:glC:render-target-mdv! background)))
+                 (cond
+                  ((procedure? foreground)
+                   (glTranslatef//checks foreground-x foreground-y 0.)
+                   (foreground))
+                  (foreground
+                   (glTranslatef//checks foreground-x foreground-y 0.)
+                   (MATURITY+1:glC:render-target-mdv! foreground)))
+                 ;; eventually pop out
+                 (glPopMatrix))))))
+    (define (ctrl! key more)
+      (case key
+        ((visible:) ;; maybe factor visibility out?
+         (if (pair? more) (set! visible (and (car more) #t)) visible))
+        ((background:)
+         (if (null? more) background
+             (let ((thing (car more)))
+               (cond
+                ((procedure? thing) (set! background thing))
+                (else (error "not a valid background" thing))))))
+        ((foreground:)
+         (if (null? more) foreground
+             (let ((thing (car more)))
+               (cond
+                ((procedure? thing) (set! foreground thing))
+                (else (error "not a valid foreground" thing))))))
+        ((align: horizontal-align:)
+         (if (pair? more)
+             (set! horizontal-align
+                   (case (car more)
+                     ((left) 'left) ((right) 'right) ((center) 'center)
+                     (else (error "alignment must be either left, right or center" (car more)))))
+             horizontal-align))
+        ((align: vertical-align:)
+         (if (pair? more)
+             (set! vertical-align
+                   (case (car more)
+                     ((top) 'top) ((bottom) 'bottom) ((center) 'center)
+                     (else (error "alignment must be either top, bottom or center" (car more)))))
+             vertical-align))
+        ((size:)
+         (if (pair? more)
+             (receive (pw ph) (apply values more)
+               (set! w pw) (set! h ph))
+             (values w h)))
+        ((position:)
+         (if (pair? more)
+             (receive (x y) (apply values more)
+               (set! shift (vector (exact->inexact x) (exact->inexact y) 0.)))
+             shift))
+        ((rot:) ;; TBD: `rotate:`? use CSS term!
+         ;;; HIER geht's weiter, da passiert 'was!:
+         ;;;
+         ;;; (magic! rot: 50 1 1 10)
+         ;;; (m2! rot:  180 30 30 180)
+         ;;; (magic! scale: 2 2)
+         ;;; (magic! position: 45 100)
+         (if (pair? more)
+             (set!
+              rot
+              (and
+               (car more)
+               (do ((given (map exact->inexact more) (cdr given))
+                    (i 0 (fx+ i 1))
+                    (result (make-vector 4 0.)))
+                   ((null? given) result)
+                 (vector-set! result i (car given)))))
+             rot))
+        ((scale:)
+         (if (pair? more)
+             (set!
+              scale
+              (and
+               (car more)
+               (do ((given (map exact->inexact more) (cdr given))
+                    (i 0 (fx+ i 1))
+                    (result (make-vector 3 0.)))
+                   ((null? given) result)
+                 (vector-set! result i (car given)))))
+             scale))
+        ((#t #f)   ;; reduce to draw thunk
+         (if key   ;; either depending on observed state
+             (lambda () (when (and visible (or background foreground)) visible ((fixed-draw))))
+             ;; otherwise resolve all references
+             (fixed-draw)))
+        (else (error "unhandled" 'button-control key))))
+    (MATURITY +2 "fresh drawing pair created" loc: 'make-guide-container-view)
+    (case-lambda
+     (() ;; draw thunk without dynamic dependencies
+      (if (%%guide-view-default-dynamic)
+          (and visible (or background foreground)
+               ;; optimized case, otherwise full exercise
+               (fixed-draw))
+          (ctrl! #t '())))
+     ((key . more) (ctrl! key more)))))
+
 (define (MATURITY+3:make-guide-button-view)
   (define (color-conv color)
     (cond ;; TBD: define consistent conversion here!
