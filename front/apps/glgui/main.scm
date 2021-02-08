57592dcbe6d5eaf7b10154f63e47af4cc382185d
Jörg F. Wittenberger <Joerg.Wittenberger@softeyes.net>
2021-02-08 20:40:55 +0100

    GUIDE: keep draft version in version test code

--- front/apps/glgui/main.scm
+++ front/apps/glgui/main.scm
@@ -29,6 +29,43 @@
 
 (include "visit-symbol-table.scm")
 
+(define (wire-persistent-file*!
+         pins ;; set of pins
+         filename
+         #!key
+         ;; encoding
+         (encode
+          (lambda pin-list
+            (object->u8vector (map (lambda (x) (x)) pin-list))))
+         (decode
+          (lambda (pin-list obj)
+            (for-each (lambda (pin val) (pin val)) pin-list (u8vector->object obj))))
+         ;; default assumes encoding as u8vector
+         (save
+          (lambda old-pins
+            ;; default: fail on encode before damaging the file
+            (let ((content (apply encode pins)))
+              (unless (u8vector? content)
+                (error "defaut `save` expects u8vector"
+                       wire-persistent-file! filename pins content))
+              (call-with-output-file filename
+                (lambda (port)
+                  (write-subu8vector content 0 (u8vector-length content) port))))))
+         (load
+          ;; NOTE: defined as variying for argument checks, see beload
+          (lambda pin-list
+            (let ((obj (read-file-as-u8vector filename)))
+              (when obj (decode pin-list obj))))))
+  ;;; TBD: maybe check filename and file access early here?
+  ;;; Nevertheless the `load` parameter (procedure) however should take
+  ;;; care anyway.
+  ;;
+  ;; NOTE: a trick: using `apply` here to enforce an argument count
+  ;; check on the `load` procedure.
+  (kick/sync! ;; a bit strict; just kick! ???
+   (lambda () (if (pair? pins) (apply load pins) (load pins))))
+  (wire! pins critical: save))
+
 (utf8string->unicode:on-encoding-error 'replace)
 
 (cond-expand
