73519729fde59296d4a3b214dc5948099c434cbf
Jörg F. Wittenberger <Joerg.Wittenberger@softeyes.net>
2021-02-10 16:06:44 +0100

    GUIDE: fix ggb2d string export

--- modules/xglgui/xglgui.scm
+++ modules/xglgui/xglgui.scm
@@ -434,7 +434,17 @@
               (error "invalid arguments" 'guide-textarea-payload key more))
             (ggb2d-display-value-on-port
              value-buffer port
-             display: (lambda (c p) (display (integer->char c) p)))
+             display:
+             (lambda (c p)
+               (cond ((##fx< c 128)
+                      (display (##integer->char c) p))
+                     ((##fx< c 2048)
+                      (display (##integer->char (##fxior (##fxarithmetic-shift-right c 6) 192)) p)
+                      (display (##integer->char (##fxior (##fxand c 63) 128)) p))
+                     (else
+                      (display (##integer->char (##fxior (##fxarithmetic-shift-right c 12) 224)) p)
+                      (display (##integer->char (##fxior (##fxand (##fxarithmetic-shift-right c 6) 63) 128)) p)
+                      (display (##integer->char (##fxior (##fxand c 63) 128)) p)))))
             (if (null? more) (get-output-string port))))
          ((text) (ggb2d-copy value-buffer))
          ((text:)
