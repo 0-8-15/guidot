af4cbd0f4f548e5a288a4dff5d922b381d715aad
Jörg F. Wittenberger <Joerg.Wittenberger@softeyes.net>
2021-02-12 21:23:58 +0100

    REPETITION: code cleanup

--- modules/repetition/repetition.scm
+++ modules/repetition/repetition.scm
@@ -2,10 +2,13 @@
  (gambit
   (declare (fixnum))
   (define-macro (abs x) `(##fxabs ,x))
-  (include "0100-repetition.scm")
-  (include "0110-ggb.scm")
-  (include "0120-ggb2d.scm")
-  (cond-expand
-   (test (include "2010-repetition.scm"))
-   (else)))
+  (define-macro (min a b) `(##fxmin ,a ,b))
+  (define-macro (max a b) `(##fxmax ,a ,b)))
  (else (error "just gambit so far")))
+
+(include "0100-repetition.scm")
+(include "0110-ggb.scm")
+(include "0120-ggb2d.scm")
+(cond-expand
+ (test (include "2010-repetition.scm"))
+ (else))
