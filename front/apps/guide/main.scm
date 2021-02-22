6a4f6f0f98e6273992ac4f7bff5e003d32a404fe
Jörg F. Wittenberger <Joerg.Wittenberger@softeyes.net>
2021-02-22 17:11:51 +0100

    APP GUIDE: moved persistent file

--- front/apps/guide/main.scm
+++ front/apps/guide/main.scm
@@ -29,46 +29,9 @@
 
 (include "visit-symbol-table.scm")
 
-(define (wire-persistent-file*!
-         pins ;; set of pins
-         filename
-         #!key
-         ;; encoding
-         (encode
-          (lambda pin-list
-            (object->u8vector (map (lambda (x) (x)) pin-list))))
-         (decode
-          (lambda (pin-list obj)
-            (for-each (lambda (pin val) (pin val)) pin-list (u8vector->object obj))))
-         ;; default assumes encoding as u8vector
-         (save
-          (lambda old-pin-values
-            ;; default: fail on encode before damaging the file
-            (let ((content (apply encode (if (pair? pins) pins (list pins)))))
-              (unless (u8vector? content)
-                (error "defaut `save` expects u8vector"
-                       wire-persistent-file! filename pins content))
-              (call-with-output-file filename
-                (lambda (port)
-                  (write-subu8vector content 0 (u8vector-length content) port))))))
-         (load
-          ;; NOTE: defined as varying for argument checks, see beloaw
-          (lambda pin-list
-            (let ((obj (read-file-as-u8vector filename)))
-              (when obj (decode pin-list obj))))))
-  ;;; TBD: maybe check filename and file access early here?
-  ;;; Nevertheless the `load` parameter (procedure) however should take
-  ;;; care anyway.
-  ;;
-  ;; NOTE: a trick: using `apply` here to enforce an argument count
-  ;; check on the `load` procedure.
-  (kick/sync! ;; a bit strict; just kick! ???
-   (lambda () (if (pair? pins) (apply load pins) (load pins))))
-  (wire! pins critical: save))
-
 (define (wire-persistent-file-as-string! pin filename)
   (MATURITY -4 "to slow, not recommended" loc: wire-persistent-file-as-string!)
-  (wire-persistent-file*!
+  (wire-persistent-file!
    pin filename
    decode: #f load:
    (lambda (pin)
